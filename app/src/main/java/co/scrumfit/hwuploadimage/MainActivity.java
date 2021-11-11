5a7a13f345889c0fe91ff9f200810876a7d9cd0e
Andres Ardila <andresardila@me.com>
2021-11-11 07:23:13 -0500

    connecting to firebase and displaying the profile image

--- app/src/main/java/co/scrumfit/hwuploadimage/MainActivity.java
+++ app/src/main/java/co/scrumfit/hwuploadimage/MainActivity.java
@@ -1,19 +1,34 @@
 package co.scrumfit.hwuploadimage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.NumberPicker;
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+
+import model.Car;
+import model.Person;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ValueEventListener {
 
     EditText edId;
     ImageView ivProfile;
@@ -54,6 +69,94 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        int id = v.getId();
+        switch (id){
+            case R.id.btnAdd:
+                addPerson(v);
+                break;
+            case R.id.btnBrowse:
+                break;
+            case R.id.btnUpload:
+                break;
+            case R.id.btnFind:
+                find();
+                break;
+
+        }
+
+    }
+
+
+
+    private void addPerson(View v) {
+
+        try {
+            ArrayList<String> hobbies = new ArrayList<>();
+            hobbies.add("Soccer");
+            hobbies.add("Cooking");
+            hobbies.add("Handball");
+            hobbies.add("Reading");
+
+            Car car1 = new Car("M300","Mazda","Mazda 3");
+
+            Person p1 = new Person(300,"Richard","im1.png",car1,hobbies);
+
+            personDatabase.child("300").setValue(p1);
+            Snackbar.make(v,"The person with the id 300 is added successfully",Snackbar.LENGTH_LONG).show();
+        } catch (Exception e){
+            Log.d("ADV_FIREBASE",e.getMessage());
+        }
+
+    }
+
+    private void find() {
+        try {
+            int id = Integer.valueOf(edId.getText().toString());
+
+            DatabaseReference personChild =
+                    FirebaseDatabase
+                    .getInstance()
+                    .getReference()
+                    .child("person")
+                    .child(String.valueOf(id));
+
+            personChild.addValueEventListener(this);
+        }catch (Exception e){
+            Log.d("ADV_FIREBASE", e.getMessage());
+        }
+
+    }
+
+    @Override
+    public void onDataChange(@NonNull DataSnapshot snapshot) {
+        if(snapshot.exists()){
+            String name = snapshot.child("name").getValue().toString();
+
+            // Accessing to the sub document (list) hobbies
+            ArrayList<String> hobbies = (ArrayList)snapshot.child("hobbies").getValue();
+            // Accessing to the sub document Car
+            HashMap car = (HashMap)snapshot.child("car").getValue();
+
+            // Accessing to the Photo URL
+            String urlPhoto = snapshot.child("photo").getValue().toString();
+
+            Log.d("ADV_FIREBASE",car.get("brand").toString());
+
+            Log.d("ADV_FIREBASE",hobbies.toString());
+
+            Log.d("ADV_FIREBASE", urlPhoto.toString());
+
+            Picasso
+                    .with(this)
+                    .load(urlPhoto)
+                    .placeholder(R.drawable.profile2)
+                    .into(ivProfile);
+        }
+
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError error) {
 
     }
 }
